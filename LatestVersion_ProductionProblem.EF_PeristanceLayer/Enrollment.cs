9bb5b2aef9453a6a70a79598b5aae207b358d7e2
LiviuSosu <sosuliviu@gmail.com>
2022-12-12 08:17:07 +0100

    added the entities

---
+++ LatestVersion_ProductionProblem.EF_PeristanceLayer/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace LatestVersion_ProductionProblem.EF_PeristanceLayer
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+}
